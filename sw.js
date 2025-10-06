8b7c9086fbed821d7d53b0b80ae8cbc7c83f46b9
Derek Aguilar <aguilarrde753@gmail.com>
2025-10-06 10:56:14 -0600

    First commit

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Estructura básica de Service Worker
+
+// 1. Nombre del caché y archivos a cachear
+const CACHE_NAME='mi-cache-v1';
+const urlsToCache=[
+    "index.js",
+    "style.css",
+    "app.js",
+    "offline.html"
+];
+
+// 2. INSTALL -> se ejecuta al instalar el SW
+self.addEventListener("install",event=>{
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
+    );
+});
+
+// 3. ACTIVATE -> se ejecuta al activarse (limpia cachés viejas)
+self.addEventListener("activate",event=>{
+    event.waitUntil(
+        caches.keys().then(keys=>
+            Promise.all(
+                keys.filter(key=>key!==CACHE_NAME)
+                    .map(key=>caches.delete(key))
+            )
+        )
+    );
+});
