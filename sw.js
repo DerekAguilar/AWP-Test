52e2ce5d3f16e49e0ed0095fc71300b40ac1d0ef
Derek Aguilar <aguilarrde753@gmail.com>
2025-10-16 20:07:11 -0600

    Revert "Changed fetch method"
    This reverts commit 29df57655fe0a1bebfc26349a0c8ae50ba2f0461.

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.addEventListener("activate",event=>{
 self.addEventListener("fetch",event=>{
     event.respondWith(
         caches.match(event.request).then(response=>{
-            return response || fetch(event.request).catch(()=>caches.match("/AWP-Test/offline.html"));
+            return response || fetch(event.request).catch(()=>caches.match("offline.html"));
         })
     )
 });
